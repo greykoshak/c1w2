6c07943a6fd0af36a29711e075113f9b54e57fb6
gk <greykoshak@gmail.com>
2019-04-08 08:26:42 +0300

    До декораторов v2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # c1w2
-Погружение в Python
+Встроенныетипы
+
+https://docs.python.org/3/library/stdtypes.html
+
+Коллекции
+
+https://docs.python.org/3/tutorial/datastructures.html
+
+Hash table
+
+https://en.wikipedia.org/wiki/Hash_table
---
+++ example_set.py
@@ -0,0 +1,15 @@
+# Через сколько итераций random выдаст повтор?
+
+import random
+
+random_set = set()
+
+while True:
+    new_number = random.randint(1, 10)
+    if new_number in random_set:
+        break
+
+    random_set.add(new_number)
+
+print(len(random_set) + 1)
+
